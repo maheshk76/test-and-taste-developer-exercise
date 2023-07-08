617d11231760010d7429742f5a518248fc355fb1
maheshk76 <mnkantariya173@gmail.com>
2023-07-08 14:29:42 +0530

    Excercise 1 - Include gravity parameter in querystring while fetching planet with moons data (GetAllPlanetsWithMoonsQueryParameters) - Introduce Gravity property inside Moon and MoonDto classes to get it from api response - Calculated average of moon gravity inside Planet class - Wrote some comments where code changes required
    Excercise 2
    - proposed another solution inside Planet class

---
+++ Test_Taste_Application/Test-Taste-Console-Application/Constants/UriPath.cs
@@ -0,0 +1,17 @@
+﻿namespace Test_Taste_Console_Application.Constants
+{
+    public static class UriPath
+    {
+        public const string BaseUri = "https://api.le-systeme-solaire.net";
+        private const string BodiesUri = "/rest/bodies";
+
+        // Included gravity parameter in data query
+        public const string GetAllPlanetsWithMoonsQueryParameters =
+            BodiesUri + "?data=id,semiMajorAxis,gravity,moons,moon,rel&filter[]=isPlanet,neq,false";
+
+        public const string GetAllMoonsWithMassQueryParameters = BodiesUri +
+                                               "?data=id,mass,massValue,massExponent,massValue&filter[]=aroundPlanet,gt,null";
+
+        public const string GetMoonByIdQueryParameters = BodiesUri + "/";
+    }
+}
---
+++ Test_Taste_Application/Test-Taste-Console-Application/Domain/Objects/Planet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+
+namespace Test_Taste_Console_Application.Domain.Objects
+{
+    public class Planet
+    {
+        public string Id { get; set; }
+        public float SemiMajorAxis { get; set; }
+        public ICollection<Moon> Moons { get; set; }
+        public float AverageMoonGravity
+        {
+            get
+            {
+                if (HasMoons())
+                {
+                    // Call Average function on Gravity property of moon
+                    return Moons.Average(x => x.Gravity);
+
+                    /* ALTERNATE : The alternate solution given below works same as Average function, 
+                     * we have to handle divide by zero exception explicitly. Average function handles that thing for us.
+                     * return Moons.Sum(x => x.Gravity) / Moons.Count;
+                     */
+                }
+                return 0f;
+            }
+        }
+
+        public Planet(PlanetDto planetDto)
+        {
+            Id = planetDto.Id;
+            SemiMajorAxis = planetDto.SemiMajorAxis;
+            Moons = new Collection<Moon>();
+            if (planetDto.Moons != null)
+            {
+                foreach (MoonDto moonDto in planetDto.Moons)
+                {
+                    Moons.Add(new Moon(moonDto));
+                }
+            }
+        }
+
+        public Boolean HasMoons()
+        {
+            return (Moons != null && Moons.Count > 0);
+        }
+    }
+}
